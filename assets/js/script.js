4f41fb6d7a3161d754649b76b5b4c0d2ca57deca
tejash <tejashyadavinfo@gmail.com>
2025-02-15 15:13:14 +0530

    add header layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,24 @@ const loadElement = document.querySelector("[data-preload]");
 window.addEventListener("load", () => {
   loadElement.classList.add("loaded");
 });
+
+// navbar
+const navbar = document.querySelector("[data-navbar]");
+const toggler = document.querySelector("[data-nav-toggler]");
+
+const toogleNavbar = () => {
+  navbar.classList.toggle("active");
+  toggler.classList.toggle("active");
+};
+
+toggler.addEventListener("click", toogleNavbar);
+
+// header
+const header = document.querySelector("[data-header]");
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
