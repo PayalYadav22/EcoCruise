64c8366fa02cdd1f56ea5401f8314b37abd2e70b
tejash <tejashyadavinfo@gmail.com>
2025-02-16 09:40:13 +0530

    add layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,10 +19,34 @@ toggler.addEventListener("click", toogleNavbar);
 
 // header
 const header = document.querySelector("[data-header]");
+const backToTop = document.querySelector("[data-back-top-btn]");
+
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 100) {
     header.classList.add("active");
+    backToTop.classList.add("active");
   } else {
     header.classList.remove("active");
+    backToTop.classList.remove("active");
   }
 });
+
+// scroll reveal
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < revealElements.length; i++) {
+    if (
+      revealElements[i].getBoundingClientRect().top <
+      window.innerHeight / 1.2
+    ) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+};
+
+window.addEventListener("scroll", scrollReveal);
+window.addEventListener("load", scrollReveal);
